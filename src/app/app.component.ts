8afe88fb751c8d68099da77b5d5a07193f5b46f8
Atchyut Nagabhairava <atchyutnagabhairava@gmail.com>
2022-02-22 18:12:44 +0530

    Added logic to capture the data and do calculation in the component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,13 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'trasactions-interview-repo';
   balance = 1000;
+
+  credit(amount: any, reason: any) {
+
+    this.balance += parseInt(amount);
+  }
+
+  debit(amount: any, reason: any) {
+    this.balance -= parseInt(amount);
+  }
 }
