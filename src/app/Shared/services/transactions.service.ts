b55f9b784feebf45e09913b1621e03182a9cae9a
Atchyut Nagabhairava <atchyutnagabhairava@gmail.com>
2022-02-22 18:40:57 +0530

    Added logic to update transactions from service

--- src/app/Shared/services/transactions.service.ts
+++ src/app/Shared/services/transactions.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TransactionsService {
 
-  constructor() { }
+  availableBalance: number = 1000;
+  captureData$: Observable<any> | undefined;
+  private captureDataSubject = new Subject<any>();
+
+
+  constructor() {
+     this.captureData$ = this.captureDataSubject.asObservable();
+  }
+
+  captureData(data: any) {
+    console.log(data);
+    this.captureDataSubject.next(data);
+  }
+
+  creditAmout() {
+    // this.availableBalance = this.availableBalance + this;
+  }
+
+   debitAmout(amount: any, reason: any) {
+    // this.availableBalance = this.availableBalance - amount;
+  }
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { TransactionsService } from './Shared/services/transactions.service';
+import { Component, Inject } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,14 +8,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'trasactions-interview-repo';
-  balance = 1000;
+  balance: any = 1000;
+  transactionArray: any = [];
+  public data: Array<any> = this.transactionArray;
+
+  constructor(private transactionService: TransactionsService) {
+    this.balance = this.transactionService.availableBalance;
+  }
+
+  ngOnChnages() {
+    this.balance = this.transactionService.availableBalance;
+  }
+
 
   credit(amount: any, reason: any) {
-
-    this.balance += parseInt(amount);
+    this.transactionService.availableBalance = this.transactionService.availableBalance + parseInt(amount);
+    this.balance = this.transactionService.availableBalance;
+    this.transactionArray = this.transactionArray.push({ amount: amount, reason: reason, type: 'credit' });
+    this.transactionService.captureData(this.data);
   }
 
   debit(amount: any, reason: any) {
-    this.balance -= parseInt(amount);
+    this.transactionService.availableBalance = this.transactionService.availableBalance - parseInt(amount);
+    this.balance = this.transactionService.availableBalance;
+    this.transactionArray = this.transactionArray.push({ amount: amount, reason: reason, type: 'debit' });
+    this.transactionService.captureData(this.data);
   }
 }
